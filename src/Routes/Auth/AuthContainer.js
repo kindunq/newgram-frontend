e562d2b65da834bae77bcb91a2df5199eaaba908
kindunq <humanb2box@gmail.com>
2020-03-11 00:10:35 +0900

    docs: addn request, createaccount func

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import AuthPresenter from './AuthPresenter';
 import useInput from '../../Hooks/useInput';
 import { useMutation } from 'react-apollo-hooks';
-import { LOG_IN } from './AuthQueries';
+import { LOG_IN, CREATE_ACCOUNT } from './AuthQueries';
+import { toast } from 'react-toastify';
 
 export default () => {
 	const [action, setAction] = useState('logIn');
@@ -10,14 +11,46 @@ export default () => {
 	const firstName = useInput('');
 	const lastName = useInput('');
 	const email = useInput('');
+
 	const [requestSecret] = useMutation(LOG_IN, {
+		update: (_, { data }) => {
+			const { requestSecret } = data;
+			if (!requestSecret) {
+				toast.error('계정이 없습니다. 계정을 만들어주세요.');
+				setTimeout(() => setAction('signUp'), 3000);
+			}
+		},
 		variables: { email: email.value }
 	});
 
-	const onLogin = e => {
+	const [createAccount] = useMutation(CREATE_ACCOUNT, {
+		variables: {
+			email: email.value,
+			username: username.value,
+			firstName: firstName.value,
+			lastName: lastName.value
+		}
+	});
+
+	const onSubmit = e => {
 		e.preventDefault();
-		if (email !== '') {
-			requestSecret();
+		if (action === 'logIn') {
+			if (email !== '') {
+				requestSecret();
+			} else {
+				toast.error('이메일주소는 필수 입니다.');
+			}
+		} else if (action === 'signUp') {
+			if (
+				email.value !== '' &&
+				username.value !== '' &&
+				firstName.value !== '' &&
+				lastName.value !== ''
+			) {
+				createAccount();
+			} else {
+				toast.error('모든 항목을 기입하세요!');
+			}
 		}
 	};
 
@@ -29,7 +62,7 @@ export default () => {
 			firstName={firstName}
 			lastName={lastName}
 			email={email}
-			onLogin={onLogin}
+			onSubmit={onSubmit}
 		/>
 	);
 };
