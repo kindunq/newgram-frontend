170965f4ea5af9693cbe2de78efd2fa65ff5d0e9
kindunq <humanb2box@gmail.com>
2020-03-16 14:51:49 +0900

    docs: add react autosize textarea

--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import TextareaAutosize from 'react-autosize-textarea';
 import FatText from '../FatText';
 import Avatar from '../Avatar';
 import { HeartFull, HeartEmpty, Comment } from '../Icons';
@@ -26,10 +27,18 @@ const Location = styled.span`
 	font-size: 12px;
 `;
 
-const Files = styled.div``;
+const Files = styled.div`
+	position: relative;
+	padding-bottom: 100%;
+`;
 
 const File = styled.img`
 	width: 100%;
+	height: 600px;
+	position: absolute;
+	top: 0;
+	background: url(${props => props.src}) center center;
+	background-size: cover;
 `;
 
 const Button = styled.span`
@@ -60,13 +69,23 @@ const Timestamp = styled.span`
 	border-bottom: ${props => props.theme.lightGreyColor} 1px solid;
 `;
 
+const Textarea = styled(TextareaAutosize)`
+	border: none;
+	width: 100%;
+	resize: none;
+	font-size: 14px;
+	&:focus {
+		outline: none;
+	}
+`;
 export default ({
 	user: { username, avatar },
 	location,
 	files,
 	isLiked,
 	likeCount,
-	createdAt
+	createdAt,
+	newComment
 }) => (
 	<Post>
 		<Header>
@@ -91,6 +110,7 @@ export default ({
 			</Buttons>
 			<FatText text={likeCount === 1 ? '1 like' : `${likeCount} likes`} />
 			<Timestamp>{createdAt}</Timestamp>
+			<Textarea placeholder={'Add a comment'} {...newComment} />
 		</Meta>
 	</Post>
 );
