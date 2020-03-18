6c0d13db1700e629530bb09b80ba511be93b64e7
kindunq <humanb2box@gmail.com>
2020-03-18 18:34:24 +0900

    docs: refactoring

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,21 +4,46 @@ import PropTypes from 'prop-types';
 import Avatar from './Avatar';
 import FatText from './FatText';
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
-const Card = styled.div``;
+const Card = styled.div`
+	${props => props.theme.whiteBox}
+	display:flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 20px;
+`;
+const EAvatar = styled(Avatar)`
+	margin-bottom: 15px;
+`;
 
-const UserCard = ({ username, isFollowing, url, isSelf }) => (
+const ELink = styled(Link)`
+	color: inherit;
+	margin-bottom: 10px;
+`;
+
+const SFatText = styled(FatText)`
+	font-weight: normal;
+	color: #999;
+	display: block;
+	margin: 5px 0;
+`;
+const UserCard = ({ firstName, username, isFollowing, avatar, isSelf }) => (
 	<Card>
-		<Avatar url={url} />
-		<FatText text={username} />
-		{!isSelf && <Button text={isFollowing ? 'Unfollow' : 'Follow'} />}
+		<EAvatar url={avatar} size={'md'} />
+		<ELink to={`/${username}`}>
+			<FatText text={username} />
+			<SFatText text={firstName} />
+		</ELink>
+
+		{!isSelf && <Button text={isFollowing ? '팔로우취소' : '팔로우'} />}
 	</Card>
 );
 
 UserCard.propTypes = {
 	username: PropTypes.string.isRequired,
 	isFollowing: PropTypes.bool.isRequired,
-	url: PropTypes.string.isRequired,
+	avatar: PropTypes.string.isRequired,
 	isSelf: PropTypes.bool.isRequired
 };
 
